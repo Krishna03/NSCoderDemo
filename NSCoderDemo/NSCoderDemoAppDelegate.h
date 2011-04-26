5dda45adb33bb1c92148be69d2f0da7972a4c59b
Matthew King <matt62king@gmail.com>
2011-04-26 16:08:30 -0500

    Initial Commit

---
+++ NSCoderDemo/NSCoderDemoAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  NSCoderDemoAppDelegate.h
+//  NSCoderDemo
+//
+//  Created by Matthew King on 4/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSCoderDemoAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
